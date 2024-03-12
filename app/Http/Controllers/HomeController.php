c77627a324b9c4ca185ed3400c0b85bc0c16f861
= <=>
2024-03-12 18:12:07 +0200

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PortalAboutUsSection;
+use App\Models\PortalBlogCategory;
+use App\Models\PortalBlogPosts;
+use App\Models\PortalContactUsInfo;
+use App\Models\PortalDepartment;
+use App\Models\PortalDoctors;
+use App\Models\PortalHomePageSlider;
+use App\Models\PortalInsurance;
+use App\Models\PortalMessages;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Validator;
+
+class HomeController extends Controller
+{
+    public function test(){
+        Auth::logout();
+        dd(Auth::user());
+        $x = Auth::attempt(['email' => 'eng.mina23@gmail.com' , 'password' => '123123']);
+        return Redirect::to('/mina');
+    }
+
+    public function test1()
+    {
+        dd(Auth::user());
+    }
+
+//
+}
