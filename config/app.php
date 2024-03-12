4db9fc7515e765498886f615ae5cc0d1fb29e872
= <=>
2024-03-12 21:28:13 +0200

    finish laravel localization

--- config/app.php
+++ config/app.php
@@ -187,7 +187,13 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         'Debugbar' => Barryvdh\Debugbar\Facades\Debugbar::class,
-        'Image' => Intervention\Image\Facades\Image::class
+        'Image' => Intervention\Image\Facades\Image::class,
+
+        'localize'                => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRoutes::class,
+        'localizationRedirect'    => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRedirectFilter::class,
+        'localeSessionRedirect'   => \Mcamara\LaravelLocalization\Middleware\LocaleSessionRedirect::class,
+        'localeCookieRedirect'    => \Mcamara\LaravelLocalization\Middleware\LocaleCookieRedirect::class,
+        'localeViewPath'          => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationViewPath::class
         // 'Example' => App\Facades\Example::class,
     ])->toArray(),
 
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,17 @@ use \App\Http\Controllers\HomeController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
+    ]
+    , function()
+{
+    Route::get('/', [HomeController::class , 'test'] );
+    Route::get('/mina', [HomeController::class , 'test1'] );
+});
 
-Route::get('/', [HomeController::class , 'test'] );
-Route::get('/mina', [HomeController::class , 'test1'] );
 //Route::get('/', function () {
 //    return view('welcome');
 //});
